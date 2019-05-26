9185a6c9b1259cbd69fbdee44a22ebfcb51d4568
Son Tran <33794783+trthanhson253@users.noreply.github.com>
2019-05-26 21:20:59 +0700

    Add files via upload

--- Demo.js
+++ Demo.js
@@ -3,32 +3,44 @@ import Header from './components/Header';
 import Product from './components/Product';
 
 class Demo extends Component {
+  onClick1(){
+    console.log('Day la 1');
+  }
+
+  onClick2(text){
+    console.log(text);
+  }
+
+  onProduct= () => {
+    console.log(this.refs.name1.value);
+  }
+
   render() {
     var products = [
       {
           id:1,
-          name:'A',
+          name: 'asdfadsf',
           price:18,
           status:true
       },
       {
           id:2,
-          name:'B',
+          name:'asdf',
           price:19,
-          status:false
+          status:true
       },
       {
           id:3,
-          name:'C',
+          name:'aasd',
           price:39,
-          status:false
+          status:true
       }
   ];
 
   var elements=products.map((product,index)=>{
     let result='';
     if(product.status){
-      result= <Product key={product.id} price={ product.price }> { product.name }</Product>
+      result=<Product key={product.id} price={ product.price }> { product.name }</Product>
     }
     return result;
 })
@@ -36,14 +48,26 @@ class Demo extends Component {
     return (
       <div>
         <Header />     
-        <div className="row">       
+        <div className="row">   
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">           
+            
+              <div className="form-group">
+                <label>Ten SP: </label>
+                <input type="text" ref="name1" />
+              </div>           
+              <button type="submit" className="btn btn-primary" onClick ={ this.onProduct }>Lưu</button>
+                      
+          </div>    
           <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
              { elements }
-             {/* <Product name="b" price="45"/> 
-             <Product name="ac" price="45"/>  */}
              
           </div>
-              
+          <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+             
+             <button type="button" className="btn btn-danger" onClick ={ this.onClick1 }>Click 1</button>
+             <button type="button" className="btn btn-danger" onClick ={ () => {this.onClick2('Son') } }>Click 2</button>
+             
+          </div>
         </div>    
         
        </div>
